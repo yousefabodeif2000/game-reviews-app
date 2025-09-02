64bc942d84e1c031293175189b73c127d7deb055
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-09-02 11:21:40 +0300

    implemented reviews controller

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using game_reviews_app.Data;
+using game_reviews_app.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace game_reviews_app.Controllers
 {
-    public class ReviewsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly DBContext _context;
+
+        public ReviewsController(DBContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PostReview(Review review)
+        {
+            _context.Reviews.Add(review);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetReview), new { id = review.Id }, review);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Review>> GetReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null) return NotFound();
+            return review;
+        }
+
+        [HttpGet("game/{gameId}")]
+        public async Task<ActionResult<IEnumerable<Review>>> GetReviewsByGame(int gameId)
+        {
+            var reviews = await _context.Reviews
+                .Where(r => r.GameId == gameId)
+                .ToListAsync();
+            return reviews;
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
